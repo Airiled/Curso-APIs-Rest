4544771165160997c1af2769c18556731391ae1f
Agustin Savino <110129241+Airiled@users.noreply.github.com>
2022-09-13 16:06:06 -0300

    Agregado de la seccion de favoritos y dinamismo en el DOM

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=6';
-const API_URL_FAVOURITES = 'https://api.thedogapi.com/v1/favourites?limit=6&api_key=live_ZCdyMjS3RJoTkiNr3xS4v5IzT94LFZg8QKZyyiCN5eR5hDiw2FsU9RD2KDoumUlm';
+const API_URL_FAVOURITES = 'https://api.thedogapi.com/v1/favourites?&api_key=live_ZCdyMjS3RJoTkiNr3xS4v5IzT94LFZg8QKZyyiCN5eR5hDiw2FsU9RD2KDoumUlm';
 const API_KEY = '&api_key=live_ZCdyMjS3RJoTkiNr3xS4v5IzT94LFZg8QKZyyiCN5eR5hDiw2FsU9RD2KDoumUlm';
 
 const spanError = document.getElementById('error');
@@ -19,17 +19,34 @@ async function renoveRandomPhoto(){
     const img5 = document.getElementById("randomDog5");
     const img6 = document.getElementById("randomDog6");
 
+    const btn1 = document.getElementById("btn1");
+    const btn2 = document.getElementById("btn2");
+    const btn3 = document.getElementById("btn3");
+    const btn4 = document.getElementById("btn4");
+    const btn5 = document.getElementById("btn5");
+    const btn6 = document.getElementById("btn6");
+
     if(res.status !== 200)
     {
         spanError.innerHTML = "Hubo un error " + res.status + data.message;
     }else{
+        console.log('Random');
         img1.src = data[0].url;
         img2.src = data[1].url;
         img3.src = data[2].url;
         img4.src = data[3].url;
         img5.src = data[4].url;
         img6.src = data[5].url;
+
+        btn1.onclick = () => saveFavouritePhotos(data[0].id);
+        btn2.onclick = () => saveFavouritePhotos(data[1].id);
+        btn3.onclick = () => saveFavouritePhotos(data[2].id);
+        btn4.onclick = () => saveFavouritePhotos(data[3].id);
+        btn5.onclick = () => saveFavouritePhotos(data[4].id);
+        btn6.onclick = () => saveFavouritePhotos(data[5].id);
     }
+
+    
 
     //CODIGO MAS DINAMICO (NO ME TERMINO DE FUNCIONAR Y NO LO PUDE ARREGLAR)
     // const imagesArray = [... images];
@@ -48,13 +65,39 @@ async function renoveRandomPhoto(){
 
 async function loadFavouritesPhotos(){
 
-    console.log('Entre a la func de favoritos!');
+    
     const res = await fetch(API_URL_FAVOURITES);
 
     if(res.status !== 200){
         spanError.innerHTML = "Hubo un error " + res.status;
     }else{
+        console.log('Favourites!');
         const data = await res.json();
+        console.log(data);
+
+        data.forEach(dog =>{
+            
+            const favouriteSection = document.querySelector('.favouriteSection');
+            const articleFavourite = document.createElement('article');
+            articleFavourite.classList.add('favouriteClass');
+
+            const imageFavourite = document.createElement('img');
+            imageFavourite.classList.add('favouriteImage');
+            imageFavourite.src = dog.image.url;
+            imageFavourite.width = 400;
+            imageFavourite.height = 350;
+
+            const btnFavourite = document.createElement('button');
+            btnFavourite.classList.add('favouriteButton');
+
+            const btnFavouriteText = document.createTextNode('Remove dog from favourites');
+
+            btnFavourite.append(btnFavouriteText);
+            articleFavourite.append(imageFavourite, btnFavourite);
+            favouriteSection.append(articleFavourite);
+
+            console.log('HOLAS');
+        });
     }
     
     console.log('Favourites');
@@ -63,14 +106,14 @@ async function loadFavouritesPhotos(){
     
 }
 
-async function saveFavouritePhotos(){
+async function saveFavouritePhotos(id){
     const res = await fetch(API_URL_FAVOURITES, {
         method: 'POST',
         headers: {  //Con el headers le aclaramos al metodo post que estamos trabajando en JSON (Se hace en el 99% de los casos)
             'Content-Type':'application/json',
         },
         body: JSON.stringify({          //Es la informacion que vamos a enviarle al backend.
-        image_id: '_zy2OcyhQ'                 //Este objeto se le manda como tipo string ya que el lenguaje en el que este escrito
+        image_id: id                    //Este objeto se le manda como tipo string ya que el lenguaje en el que este escrito
                                         //el API al que se lo vamos a mandar no sabemos en que lenguaje de programacion va a estar
                                         //realizado.
 
@@ -81,6 +124,7 @@ async function saveFavouritePhotos(){
         spanError.innerHTML = "Hubo un error " + res.status;
     }else{
         const data = await res.json();
+        console.log('Agregado con exito!');
     }
 }
 
